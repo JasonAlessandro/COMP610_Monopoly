db84b9bbf1401240714fe1e52d96125dfb68abbb
Takixu <103513597+Takixu@users.noreply.github.com>
2023-06-07 17:50:34 +1200

    Update Game.java
    change the end game in 20 ends. Now the game ends when there is only one player left

--- Game.java
+++ Game.java
@@ -2,37 +2,12 @@ package com.mycompany.monopoly;
 
 import java.util.ArrayList;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 public class Game {
 
-    public static String CheckMostMoneytoString(ArrayList<Player> players) {
-
-        String winner = " ";
-
-        int highestBalance = 0;
-        for (int i = 0; i < players.size(); i++) {
-            if (players.get(i).getProperty().getMoney() > highestBalance) {
-                highestBalance = players.get(i).getProperty().getMoney();
-                winner = players.get(i).getName();
-            }
-        }
-
-        return winner + " is the winner!";
-
-    }
-
-    public static void checkTurn(int turns, ArrayList<Player> players, JFrame gameWindow) {
-        if (turns == 2) {
-
-            JOptionPane.showMessageDialog(gameWindow, CheckMostMoneytoString(players), "Winner", JOptionPane.INFORMATION_MESSAGE);
-
-            gameWindow.dispose();
-            FileIO.incrementGamesPlayed();
-        }
-    }
-}
-/*private static int currentPlayer = 0;
+    private static int currentPlayer = 0;
 
     public static void removePlayer(ArrayList<Player> players, int playerNum, Property[] properties) {
         // property
@@ -53,14 +28,48 @@ public class Game {
     public static void checkRemovePlayer(ArrayList<Player> players, Property[] properties) {
         for (int i = 0; i < players.size(); i++) {
             if (players.get(i).getProperty().getMoney() < 0) {
+                JFrame lossWindow = new JFrame("Player Lost");
+            lossWindow.setSize(300, 200);
+            lossWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+            // Create a JLabel with the loss message and add it to the JFrame
+            JLabel lossMessage = new JLabel(players.get(i).getName() + " has lost!");
+            lossMessage.setHorizontalAlignment(JLabel.CENTER);
+            lossWindow.add(lossMessage);
+
+            // Display the JFrame
+            lossWindow.setVisible(true);
                 removePlayer(players, i, properties);
                 break;
             }
         }
     }
 
+   
     public static void checkWinner(ArrayList<Player> players) {
-        if (players.size() == 1) {
-            System.out.println("Congratulations, " + players.get(0).getName() + "! You are the winner!");
-        }
-    }*/
+    // If there's only one player left, they are the winner
+    if (players.size() == 1) {
+        // Create a new JFrame and set its properties
+        JFrame winWindow = new JFrame("Winner");
+        winWindow.setSize(300, 200);
+        winWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        // Create a JLabel with the win message and add it to the JFrame
+        JLabel winMessage = new JLabel(players.get(0).getName() + " has won!");
+        winMessage.setHorizontalAlignment(JLabel.CENTER);
+        winWindow.add(winMessage);
+
+        // Display the JFrame
+        winWindow.setVisible(true);
+
+        FileIO.incrementGamesPlayed();
+    }
+}
+
+
+   public static void checkTurn(int turns, ArrayList<Player> players, Property[] properties) {
+    // After each turn, check if any player should be removed, and if there's a winner
+    checkRemovePlayer(players, properties);
+    checkWinner(players);
+}
+}
