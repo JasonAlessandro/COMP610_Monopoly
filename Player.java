4edb79c0cbadf000ef74b01aabe9a18d3da41b6f
Takixu <103513597+Takixu@users.noreply.github.com>
2023-06-08 21:59:07 +1200

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,4 +1,4 @@
-package com.mycompany.monopoly;
+package monopoly;
 
 import java.awt.Color;
 import java.util.HashSet;
@@ -6,19 +6,21 @@ import java.util.HashSet;
 public class Player {
 
     private String name;
+    public int id;
     private Color color;
     private int position;
     private PlayerProperty property;
     private boolean hasEndedTurn;
     private HashSet<Integer> ownedProperties;
 
-    public Player(String name, Color color, int startingMoney) {
+    public Player(String name, Color color, int startingMoney,boolean hasEndedTurn,int id) {
         this.name = name;
         this.color = color;
         this.position = 0;
         this.property = new PlayerProperty(startingMoney);
-        this.hasEndedTurn = false;
+        this.hasEndedTurn = hasEndedTurn;
         this.ownedProperties = new HashSet<>();
+        this.id = id;
     }
 
     public String getName() {
@@ -60,5 +62,7 @@ public class Player {
     public void addProperty(int position) {
         ownedProperties.add(position);
     }
+    
+    
 
 }
