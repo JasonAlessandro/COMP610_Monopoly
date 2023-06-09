9ada8b4c38496efc2db88f891204e943268e4c4c
JasonAlessandro <105477409+JasonAlessandro@users.noreply.github.com>
2023-06-09 17:23:43 +0700

    Update PlayerProperty.java

--- PlayerProperty.java
+++ PlayerProperty.java
@@ -1,10 +1,11 @@
-package com.mycompany.monopoly;
+package monopoly;
 
 public class PlayerProperty {
 
     private int money;
     private int numberOfProperties;
 
+    //Constructor and Get Set methods
     public PlayerProperty(int initialMoney) {
         this.money = initialMoney;
         this.numberOfProperties = 0;
@@ -18,15 +19,22 @@ public class PlayerProperty {
         return numberOfProperties;
     }
 
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
     public void addMoney(int amount) {
+        //Increases the player's money by the specified amount
         this.money += amount;
     }
 
     public void subtractMoney(int amount) {
+        // Decreases the player's money by the specified amount.
         this.money -= amount;
     }
 
     public void addProperty() {
+        // Increases the number of properties owned by the player by 1.
         this.numberOfProperties++;
     }
 
