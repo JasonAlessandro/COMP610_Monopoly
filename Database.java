1614839db7a8cc09309943bec62096507b425b5b
Takixu <103513597+Takixu@users.noreply.github.com>
2023-06-09 15:55:34 +1200

    Update Database.java
    redo the horse table

--- Database.java
+++ Database.java
@@ -39,18 +39,26 @@ public class Database {
     }
 
     private static void createHorseTable() {
-        try (Connection connection = DriverManager.getConnection("jdbc:derby:app;create=true");
-             Statement statement = connection.createStatement()) {
+    try (Connection connection = DriverManager.getConnection("jdbc:derby:app;create=true");
+         Statement statement = connection.createStatement()) {
 
-            String createTableSQL = "CREATE TABLE Horse (" +
-                    "horseId INT PRIMARY KEY)";
-            statement.executeUpdate(createTableSQL);
+        String createTableSQL = "CREATE TABLE Horse (" +
+                "horseId INT PRIMARY KEY, " +
+                "gamesWon INT DEFAULT 0)";
+        statement.executeUpdate(createTableSQL);
 
-            System.out.println("Horse table created successfully.");
-        } catch (SQLException e) {
-            e.printStackTrace();
+        
+        for (int i = 1; i <= 6; i++) {
+            String insertHorseSQL = "INSERT INTO Horse (horseId) VALUES (" + i + ")";
+            statement.executeUpdate(insertHorseSQL);
         }
+
+        System.out.println("Horse table created successfully.");
+    } catch (SQLException e) {
+        e.printStackTrace();
     }
+}
+
 
     private static void createLandsTable() {
         try (Connection connection = DriverManager.getConnection("jdbc:derby:app;create=true");
